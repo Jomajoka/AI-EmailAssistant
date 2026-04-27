07dd1e30c07b6050ce34582ca5f049b5d639789e
Jomajoka <joelmkalloor@gmail.com>
2026-04-27 19:00:33 +0530

    fix for invalid auth state

--- app/api.py
+++ app/api.py
@@ -8,27 +8,19 @@ from app.auth.routes import router as auth_router
 from app.routers.sync import router as sync_router
 import os
 
-
-from fastapi import FastAPI
-from starlette.middleware.sessions import SessionMiddleware
-from fastapi.middleware.cors import CORSMiddleware
-from app.database.db import init_db
-from app.routers.process import router as process_router
-from app.routers.data import router as data_router
-from app.auth.routes import router as auth_router
-from app.routers.sync import router as sync_router
-import os
-
 def create_app():
     init_db()
 
     app = FastAPI()
 
+    is_production = os.getenv("ENVIRONMENT") == "production"
+
     app.add_middleware(
         SessionMiddleware,
         secret_key=os.getenv("SECRET_KEY"),
-        same_site="lax",
-        https_only=os.getenv("ENVIRONMENT") == "production"
+        same_site="none" if is_production else "lax",
+        https_only=is_production,
+        max_age=3600
     )
 
     app.add_middleware(
@@ -51,4 +43,4 @@ def create_app():
     app.include_router(process_router)
     app.include_router(data_router)
 
-    return app
+    return app  
